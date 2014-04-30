d3571f05b6af7e833b1605262744bb97cd97c59c
cooloon <cooloon@gmail.com>
2014-04-30 14:13:09 +0900

    - initメソッドの外枠を実装

--- Classes/GameLayer.cpp
+++ Classes/GameLayer.cpp
@@ -17,6 +17,66 @@ bool GameLayer::init()
         return false;
     }
     
+    // member variables
+    _screenSize = Director::getInstance()->getVisibleSize();
+    _drawing = false;
+    _minimumLineLength = _screenSize.width * .07;
+    _state = kGameIntro;
+    
+    // prepare sprites
+    createGameScreen();
+    createParticles();
+    createStarGrid();
+    
+    // enable touch
+    EventListenerTouchAllAtOnce *touches = EventListenerTouchAllAtOnce::create();
+    touches->onTouchesBegan = CC_CALLBACK_2(GameLayer::onTouchesBegan, this);
+    touches->onTouchesMoved = CC_CALLBACK_2(GameLayer::onTouchesMoved, this);
+    touches->onTouchesEnded = CC_CALLBACK_2(GameLayer::onTouchesEnded, this);
+    this->getEventDispatcher()->addEventListenerWithSceneGraphPriority(touches, this);
+    
+    // update
+    this->scheduleUpdate();
+    
     return true;
 }
 
+void GameLayer::createGameScreen(void)
+{
+    // 背景
+    Sprite *background = Sprite::create("bg.png");
+    background->setPosition(_screenSize.width/2, _screenSize.height/2);
+    this->addChild(background, kBackGround);
+    
+    
+}
+
+void GameLayer::createParticles(void)
+{
+    
+}
+
+void GameLayer::createStarGrid(void)
+{
+    
+}
+
+void GameLayer::update(float dt)
+{
+    
+}
+
+void onTouchesBegan(const std::vector<Touch*>& touches, Event *unused_event)
+{
+    
+}
+
+void onTouchesMoved(const std::vector<Touch*>& touches, Event *unused_event)
+{
+    
+}
+
+void onTouchesEnded(const std::vector<Touch*>& touches, Event *unused_event)
+{
+    
+}
--- Classes/GameLayer.h
+++ Classes/GameLayer.h
@@ -5,6 +5,14 @@
 
 USING_NS_CC;
 
+typedef enum GameState : int {
+    kGameIntro,
+} GameState;
+
+typedef enum zPosition : int {
+    kBackGround,
+} GameState;
+
 class GameLayer : public cocos2d::Layer
 {
 public:
@@ -12,7 +20,21 @@ public:
     CREATE_FUNC(GameLayer);
     virtual bool init();
     
+    virtual void onTouchesBegan(const std::vector<Touch*>& touches, Event *unused_event);
+    virtual void onTouchesMoved(const std::vector<Touch*>& touches, Event *unused_event);
+    virtual void onTouchesEnded(const std::vector<Touch*>& touches, Event *unused_event);
+    
+    void update(float dt);
+    
 private:
+    void createGameScreen(void);
+    void createParticles(void);
+    void createStarGrid(void);
+    
+    Size _screenSize;
+    bool _drawing;
+    float _minimumLineLength;
+    GameState _state;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
