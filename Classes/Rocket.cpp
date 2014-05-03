d130d8ee207a59bacea49b70a95c03dda25d2bd9
cooloon <cooloon@gmail.com>
2014-05-03 14:42:05 +0900

    - Rocketクラスの初期コード記述

---
+++ Classes/Rocket.cpp
@@ -0,0 +1,86 @@
+//
+//  Rocket.cpp
+//  20_RocketThrough
+//
+//  Created by Shin Takano on 2014/05/03.
+//
+//
+
+#include "Rocket.h"
+
+Rocket::Rocket():
+_targetRotation(0),
+_dr(0),
+_ar(0),
+_vr(0),
+_rotationSpring(.2),
+_rotationDamping(.5),
+_pivot(Point(-1, -1)),
+_rotationOrientation(RotationOrientation::NONE)
+{
+    this->reset();
+}
+
+void Rocket::reset () {
+    
+    _speed = 50;
+	_pivot = Point(-1,1);
+    _rotationOrientation = RotationOrientation::NONE;
+    this->setRotation(-90);
+	_targetRotation = -90;
+    float angle = CC_DEGREES_TO_RADIANS(this->getRotation());
+	_vector = Point(_speed * cos( angle ),
+                    -_speed * sin( angle ));
+}
+
+Rocket *Rocket::create()
+{
+    Rocket *rocket = new Rocket();
+    if (rocket && rocket->initWithSpriteFrameName("rocket.png")) {
+        rocket->autorelease();
+        rocket->setRadius(rocket->getContentSize().width/2);
+        return rocket;
+    }
+    CC_SAFE_DELETE(rocket);
+    return NULL;
+}
+
+bool Rocket::isCollidedWithSides()
+{
+    Size screenSize = CCDirector::getInstance()->getWinSize();
+    
+    if (_position.x > screenSize.width - _radius) {
+        _position.x =  screenSize.width - _radius;
+        _rotationOrientation = RotationOrientation::NONE;
+        _vector = Point(this->getVector().x * -1, this->getVector().y);
+        this->setRotationFromVector();
+        return true;
+    }
+    
+    if (_position.x < _radius) {
+        _position.x =  _radius;
+       	_rotationOrientation =  RotationOrientation::NONE;
+        _vector =  Point(this->getVector().x * -1, this->getVector().y);
+        this->setRotationFromVector();
+        return true;
+        
+    }
+    
+    if (_position.y < _radius) {
+        _position.y = _radius;
+       	_rotationOrientation = RotationOrientation::NONE;
+        _vector =  Point(this->getVector().x, this->getVector().y * -1);
+        this->setRotationFromVector();
+        return true;
+    }
+    
+    if (_position.y > screenSize.height - _radius) {
+        _position.y =  screenSize.height - _radius;
+       	_rotationOrientation = RotationOrientation::NONE;
+        _vector =  Point(this->getVector().x, this->getVector().y * -1);
+        this->setRotationFromVector();
+        return true;
+    }
+    
+    return false;
+}
---
+++ Classes/Rocket.h
@@ -0,0 +1,59 @@
+//
+//  Rocket.h
+//  20_RocketThrough
+//
+//  Created by Shin Takano on 2014/05/03.
+//
+//
+
+#ifndef ___0_RocketThrough__Rocket__
+#define ___0_RocketThrough__Rocket__
+
+#include "cocos2d.h"
+#include "GameSprite.h"
+
+USING_NS_CC;
+
+enum class RotationOrientation : int {
+    NONE,
+    CLOCKWIZE,
+    COUNTER_CLOCKWIZE
+};
+
+class Rocket : public GameSprite
+{
+public:
+    Rocket(void);
+    Rocket *create();
+    
+    void reset(void);
+    bool isCollidedWithSides(void);
+    
+    inline void setTargetRotation(float value) {
+        _targetRotation = value;
+    }
+    
+    inline void setRotationFromVector() {
+        this->setTargetRotation(CC_RADIANS_TO_DEGREES(atan2(-_vector.y, _vector.x)));
+    }
+    
+    CC_SYNTHESIZE(Point, _vector, Vector);
+    CC_SYNTHESIZE(Point, _pivot, Pivot);
+    CC_SYNTHESIZE(float, _speed, Speed);
+    CC_SYNTHESIZE(RotationOrientation, _rotationOrientation, RotationOrientation);
+    
+protected:
+    
+    
+private:
+	
+	float _targetRotation;
+	float _dr;
+	float _ar;
+	float _vr;
+	float _rotationSpring;
+	float _rotationDamping;
+    
+};
+
+#endif /* defined(___0_RocketThrough__Rocket__) */
